0f82047c6243e9488e9864aa664e05f0821b91c8
luojiahuan <841254332@qq.com>
2016-09-23 16:34:12 +0800

    index_kmp

--- KMP-string/main.cpp
+++ KMP-string/main.cpp
@@ -24,14 +24,33 @@ void get_next(string T, int next[]){
     }
 }
 
+int Index_kmp(string s, string t, int next[]){
+    int i=0, j=0;
+    while (i<s.length()&&j<t.length()) {
+        if (j==-1||s[i]==t[j]) {
+            i++;
+            j++;
+        }else{
+            j=next[j]-1;
+        }
+    }
+    if(j>=t.length()){
+        cout<<i-t.length()<<endl;
+        return 0;
+    }else{
+        return -1;
+    }
+}
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     string s = "abababcdeabcfghojk";
-    string t = "abaabc";
+    string t = "abc";
     int next[t.length()];
     get_next(t, next);
-    for (int i = 0; i<t.length(); i++) {
-        cout<<next[i]<<endl;
-    }
+//    for (int i = 0; i<t.length(); i++) {
+//        cout<<next[i]<<endl;
+//    }
+    Index_kmp(s, t, next);
     return 0;
 }
